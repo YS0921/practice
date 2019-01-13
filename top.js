2f86c9bd13ae165cb21526abc0a2233199c97646
EC2 Default User <ec2-user@ip-172-31-8-43.us-east-2.compute.internal>
2019-01-13 12:45:41 +0000

    ヘッダにスクロール時のアニメーションを追加

--- top.js
+++ top.js
@@ -1,6 +1,20 @@
 
 $(function(){
-    $('.slider').slick({
+    var $win = $(window),
+    $header = $('header'),
+    animationClass = 'fixed',
+    slider = $('.slider');
+
+    $win.on('load scroll', function() {
+        var value = $(this).scrollTop();
+        if ( value > 100 ) {
+          $header.addClass(animationClass);
+        } else {
+          $header.removeClass(animationClass);
+        }
+      });
+    
+    slider.slick({
         dots: true,
         autoplay: true,
         arrows: false
